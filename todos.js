b812679e5bcab42c4d0757db21069b0a2c5ca72e
keithchasen <keithchasen89@gmail.com>
2024-11-09 22:11:05 +0100

    basic event listeners and functions

--- todos.js
+++ todos.js
@@ -3,3 +3,26 @@ const todos = ['walk the dog', 'water the plants', 'sand the chairs'];
 const addTodoInput = document.getElementById('todo-input');
 const addTodoButton = document.getElementById('add-todo-btn');
 const todoList = document.getElementById('todo-list');
+
+for (const todo in todos) {
+	todoList.appendChild(renderTodoInReadMode(todo));
+}
+
+addTodoInput.addEventListener('input', () => {
+	addTodoButton.disabled = addTodoInput.ariaValueMax.length < 3;
+});
+
+addTodoInput.addEventListener('keydown', ({ key }) => {
+	if (key === 'Enter' && addTodoInput.ariaValueMax.length >= 3) {
+		addTodo();
+	}
+});
+
+addTodoButton.addEventListener('click', addTodo);
+
+function renderTodoInReadMode(todo) {
+	//todo: implement
+}
+function addTodo() {
+	//todo: implement
+}
