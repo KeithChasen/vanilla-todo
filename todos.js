941cc3553725ea55d54ffa2f1e8a73131c427e5d
keithchasen <keithchasen89@gmail.com>
2024-11-09 20:22:34 +0100

    mock arrays of todos and references to the dom elements

---
+++ todos.js
@@ -0,0 +1,5 @@
+const todos = ['walk the dog', 'water the plants', 'sand the chairs'];
+
+const addTodoInput = document.getElementById('todo-input');
+const addTodoButton = document.getElementById('add-todo-btn');
+const todoList = document.getElementById('todo-list');
