d67a80fbcfbcc83a2411878f0741d3009cd1bae6
keithchasen <keithchasen89@gmail.com>
2024-11-09 22:35:50 +0100

    basic styles for todo / rendering todo list

--- todos.js
+++ todos.js
@@ -4,8 +4,8 @@ const addTodoInput = document.getElementById('todo-input');
 const addTodoButton = document.getElementById('add-todo-btn');
 const todoList = document.getElementById('todo-list');
 
-for (const todo in todos) {
-	// todoList.appendChild(renderTodoInReadMode(todo));
+for (const todo of todos) {
+	todoList.appendChild(renderTodoInReadMode(todo));
 }
 
 addTodoInput.addEventListener('input', () => {
@@ -21,8 +21,32 @@ addTodoInput.addEventListener('keydown', ({ key }) => {
 addTodoButton.addEventListener('click', addTodo);
 
 function renderTodoInReadMode(todo) {
-	//todo: implement
+	const li = document.createElement('li');
+	const span = document.createElement('span');
+	span.textContent = todo;
+	span.addEventListener('dblclick', () => {
+		const idx = todos.indexOf(todo);
+		todoList.replaceChild(renderTodoInEditMode(todo), todoList.childNodes[idx]);
+	});
+	li.append(span);
+
+	const button = document.createElement('button');
+	button.textContent = 'Done';
+	button.addEventListener('click', () => {
+		const idx = todos.indexOf(todo);
+		removeTodo(idx);
+	});
+	li.append(button);
+	return li;
+}
+
+function renderTodoInEditMode(todo) {
+	//todo: inplement
 }
 function addTodo() {
+	//todo: implement
+}
+
+function removeTodo(idx) {
 	//todo: implement
 }
