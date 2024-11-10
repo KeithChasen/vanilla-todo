84630636e20d55006118b2cb8f9aa6861776f0bb
keithchasen <keithchasen89@gmail.com>
2024-11-10 22:00:19 +0100

    render todo in edit mode function

--- todos.js
+++ todos.js
@@ -41,9 +41,36 @@ function renderTodoInReadMode(todo) {
 }
 
 function renderTodoInEditMode(todo) {
-	//todo: inplement
+	const li = document.createElement('li');
+
+	const input = document.createElement('input');
+	input.type = 'text';
+	input.value = todo;
+	li.append(input);
+
+	const saveBtn = document.createElement('button');
+	saveBtn.textContent = 'Save';
+	saveBtn.addEventListener('click', () => {
+		const idx = todos.indexOf(todo);
+		updateTodo(idx, input.value);
+	});
+	li.append(saveBtn);
+
+	const cancelBtn = document.createElement('button');
+	cancelBtn.textContent = 'Cancel';
+	cancelBtn.addEventListener('click', () => {
+		const idx = todos.indexOf(todo);
+		todoList.replaceChild(renderTodoInReadMode(todo), todoList.childNodes[idx]);
+	});
+	li.append(cancelBtn);
+
+	return li;
 }
 function addTodo() {
+	//todo: implement
+}
+
+function updateTodo(idx, description) {
 	//todo: implement
 }
 
